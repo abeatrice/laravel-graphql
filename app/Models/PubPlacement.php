a0a2205d4991b85843619657948755749c026d14
andrew beatrice <abeatrice.mail@gmail.com>
2021-11-08 22:08:22 -0800

    pubp model

---
+++ app/Models/PubPlacement.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PubPlacement extends Model
+{
+    use HasFactory;
+
+    protected $table = 'PubPlacement';
+}
